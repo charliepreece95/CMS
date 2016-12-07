3ab2c5428be76dc83b724a7d60ac50a2cd0d053a
p15221433 <p15221433@GWHXX582WIN006W>
2016-12-07 14:11:47 +0000

    Add project files.

---
+++ BCBackOffice/frmMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary; //My own library
+
+namespace BCBackOffice
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPopulate_Click(object sender, EventArgs e)
+        {
+            //call the DisplayCompanies function
+            lblDisplay.Text = DisplayCompanies() + " Found";
+        }
+
+        Int32 DisplayCompanies()
+        {
+            //create an instance of the company collection 
+            clsCompanyCollection MyCompany = new clsCompanyCollection();
+            //find all the companies
+            MyCompany.FindAllCompanies();
+            //set the data source of the list box
+            listCompanies.DataSource = MyCompany.ListCompany;
+            //set the text to be displayed 
+            listCompanies.DisplayMember = "CompanyName";
+            //set the primary key value
+            listCompanies.ValueMember = "CompanyID";
+            //return the count of records in the list
+            return MyCompany.Count;
+        }
+    }
+}
---
+++ BCFrontEnd/Default.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Business_Card_Prototype.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <br />
+    <asp:ListBox ID="listCompanies" runat="server" Height="82px" Width="197px"></asp:ListBox>
+    <br />
+    <br />
+    <asp:Button ID="btnPopulate" runat="server" OnClick="btnPopulate_Click" Text="Populate" />
+    <br />
+    <br />
+    <asp:Label ID="lblDisplay" runat="server" Text="error"></asp:Label>
+    <br />
+</asp:Content>
+
+<!-- Main page -->
